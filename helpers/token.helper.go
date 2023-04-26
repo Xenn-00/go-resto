175ff6ea6657995850bd75c4b2f8ca78c8fb3823
Xenn-00 <node.xenon@gmail.com>
2023-04-26 08:14:15 +0700

    add hash and verify password

--- helpers/token.helper.go
+++ helpers/token.helper.go
@@ -1,1 +1,23 @@
 package helpers
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
+}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(givenPassword), []byte(userPassword))
+	if err != nil {
+		return false, "incorect email or password"
+	}
+	return true, ""
+}
