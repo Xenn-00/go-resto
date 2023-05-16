7e13c121586a725e8470ca42ce0aa44dd24d0142
Xenn-00 <node.xenon@gmail.com>
2023-05-16 10:58:39 +0700

    minor update

--- models/menu.model.go
+++ models/menu.model.go
@@ -10,8 +10,8 @@ type Menu struct {
 	ID         primitive.ObjectID `bson:"_id" json:"_id"`
 	Name       *string            `bson:"name" json:"name" validate:"required"`
 	Category   *string            `bson:"category" json:"category" validate:"required"`
-	Start_date time.Time          `bson:"start_date" json:"start_date"`
-	End_date   time.Time          `bson:"end_date" json:"end_date"`
+	Start_date *time.Time         `bson:"start_date" json:"start_date"`
+	End_date   *time.Time         `bson:"end_date" json:"end_date"`
 	Created_at time.Time          `bson:"created_at" json:"created_at"`
 	Updated_at time.Time          `bson:"updated_at" json:"updated_at"`
 	Menu_Id    string             `bson:"menu_id" json:"menu_id"`
