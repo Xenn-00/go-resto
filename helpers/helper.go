bb401fc107c6e6f1ef0d2c1cd8d2a7bdeda48887
Xenn-00 <node.xenon@gmail.com>
2023-05-16 10:58:55 +0700

    add some function

--- helpers/helper.go
+++ helpers/helper.go
@@ -2,7 +2,10 @@ package helpers
 
 import (
 	"log"
+	"math"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -20,4 +23,16 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
 		return false, "incorect email or password"
 	}
 	return true, ""
+}
+
+func ToFixed(num float64, precision int) float64 {
+	return math.Round(num/float64(precision)) * float64(precision)
+}
+
+func ItemByOrder(id string) (OrderItem []primitive.M, err error) {
+	return nil, nil
+}
+
+func InTimeSpan(start, end, check time.Time) bool {
+	return start.After(time.Now()) && end.After(start)
 }
